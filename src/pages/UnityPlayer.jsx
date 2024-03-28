13a9abd9c9f3930af05b2a35b6ff22a43152c269
Ninad <ninadkarkhanis2003@gmail.com>
2024-03-28 19:58:01 +0530

    new update
    new apk and windows exe

--- src/pages/UnityPlayer.jsx
+++ src/pages/UnityPlayer.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React from "react";
 
 function GameHTMLPage() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh' }}>
-    <iframe frameborder="0" src="https://itch.io/embed-upload/10043941?color=68BDFA" allowfullscreen="" width="1080" height="1940" ><a href="https://nnk2003.itch.io/treasure-quest">Play Treasure Quest - PC on itch.io</a></iframe>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "90vh",
+      }}
+    >
+      <iframe
+        frameborder="0"
+        src="https://itch.io/embed-upload/10044425?color=68BDFA"
+        allowfullscreen=""
+        width="1080"
+        height="1940"
+      >
+        <a href="https://nnk2003.itch.io/treasure-quest">
+          Play Treasure Quest - PC on itch.io
+        </a>
+      </iframe>
     </div>
   );
 }
